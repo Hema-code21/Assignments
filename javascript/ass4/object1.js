2f04351e721167ff1fd0e210cabdbf9351f20235
Hema-code21 <bhema3824@gmail.com>
2026-03-05 19:59:07 +0530

    Javascript

---
+++ javascript/ass4/object1.js
@@ -0,0 +1,50 @@
+// student object
+let student={
+    name:"Hema",
+    course:"Btech",
+    branch:"CSE",
+    rollNo:518,
+    age:19,
+    address :"Telangana"
+}
+console.log("Name:",student.name);
+console.log("Course:",student.course);
+console.log("Branch:",student.branch);
+console.log("rollno:",student.rollNo);
+console.log("age:",student.age);
+console.log("address:",student.address);
+// bus object;
+let bus={
+    busName:"TSRTC",
+    driver:"Revanth",
+    route:"Nizampet-Mallampet",
+    routeNo:224
+}
+console.log("Busname:",bus.busName);
+console.log("Driver:",bus.driver);
+console.log("Route:",bus.route);
+console.log("Route no:",bus.routeNo);
+//employee object
+let employee={
+    employeeId:15,
+    name:"Revanth",
+    salary:30000,
+    address:"Hyderabad"
+}
+console.log("Employee ID:",employee.employeeId);
+console.log("Name:",employee.name);
+console.log("Salary:",employee.salary);
+console.log("Address:",employee.address);
+// mobile object
+let mobile={
+    type:"Android",
+    brand:"Realme",
+    model:"realmeP1",
+    price:15000,
+    ram:"8GB"
+}
+console.log("Mobile type:",mobile.type);
+console.log("Brand:",mobile.brand);
+console.log("Model:",mobile.model);
+console.log("Price:",mobile.price);
+console.log("ram:",mobile.ram);
---
+++ javascript/ass4/object2.js
@@ -0,0 +1,13 @@
+let student1={
+    name:"Hema",
+    course:"BTech",
+    branch:"CSE",
+    marks:[50,48,49,40],
+    address:"Hyderabad"
+}
+console.log("Student name:",student1.name);
+console.log("Course:",student1.course);
+console.log("Branch:",student1.branch);
+console.log("Marks:",student1.marks);
+console.log("Address:",student1.address);
+;
